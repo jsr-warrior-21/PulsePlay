c25e492eee33102c97df70059d855da2e5490aa1
Arvind <jsrarvind12@gmail.com>
2026-04-09 00:51:29 +0530

    Fixing

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -6,7 +6,7 @@ export const getSecureUrl = (url) => {
 };
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:8000/api/v1",
+    baseURL: import.meta.env.VITE_BACKEND_URL,
     withCredentials: true,
 });
 
