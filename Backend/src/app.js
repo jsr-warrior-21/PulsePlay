f6ea5e2662eb31d45a691a45b961816831d3a4d9
Arvind <jsrarvind12@gmail.com>
2026-04-08 23:13:53 +0530

    fixing

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -10,10 +10,9 @@ app.use(
     origin: process.env.CORS_ORIGIN,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With", "Accept"],
   })
 );
-
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
